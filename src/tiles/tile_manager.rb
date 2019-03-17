aef5c8db26a1c2b6d52f4a88bc1e6678a76a89fe
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-17 22:36:31 +0000

    Document TileManager

--- src/tiles/tile_manager.rb
+++ src/tiles/tile_manager.rb
@@ -3,11 +3,17 @@ require_relative 'tile_set'
 
 module ScenicRoute
   module Tiles
+    ##
+    # A static class which handles loading of tile sets.
     class TileManager
+      ##
+      # A mapping of known tile set names to their filepaths and tile sizes.
       TILE_SET_NAMES = {
         track: ['res/roads.png', 64, 64]
       }
 
+      ##
+      # A mapping of known tile set names to their tile definition hashes.
       TILE_SET_DEFINITIONS = {
         track: {
           # The bend directions specify the sides on which the bend is open
@@ -22,7 +28,19 @@ module ScenicRoute
         }
       }
 
-      # Loads a tile set from a file.
+      ##
+      # Loads a tile set from a file. Once loaded, the tile set is memoised
+      # to reduce disk usage.
+      #
+      # @param [Symbol, String] name The name of the tile set, either as a
+      #   symbol key from {TILE_SET_NAMES} or as a string filepath. Note that,
+      #   if using a symbol, all future parameters' values are ignored.
+      # @param [Numeric] width The width of each tile.
+      # @param [Numeric] height The height of each tile.
+      # @param [Hash] tile_definitions The tile definitions for the resulting
+      #   tile set to use.
+      #
+      # @return [TileSet] The loaded tile set.
       def self.tile_set(name, width=nil, height=nil, tile_definitions=nil)
         @@loaded_tile_sets ||= {}
 
