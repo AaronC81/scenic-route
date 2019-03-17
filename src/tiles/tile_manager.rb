fbc9bcbad18f3e19bcee513ad82c1f48ade31259
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-17 19:46:58 +0000

    Move tile files into their own module

--- src/tiles/tile_manager.rb
+++ src/tiles/tile_manager.rb
@@ -2,41 +2,43 @@ require 'gosu'
 require_relative 'tile_set'
 
 module ScenicRoute
-  class TileManager
-    TILE_SET_NAMES = {
-      track: ['res/spritesheet_tiles.png', 128, 128]
-    }
-
-    TILE_SET_DEFINITIONS = {
-      track: {
-        straight_top: 0,
-        south_east_bend: 22
+  module Tiles
+    class TileManager
+      TILE_SET_NAMES = {
+        track: ['res/spritesheet_tiles.png', 128, 128]
       }
-    }
 
-    # Loads a tile set from a file.
-    def self.tile_set(name, width=nil, height=nil, tile_definitions=nil)
-      @@loaded_tile_sets ||= {}
+      TILE_SET_DEFINITIONS = {
+        track: {
+          straight_top: 0,
+          south_east_bend: 22
+        }
+      }
 
-      # See if we have a memoised copy of this tile set
-      return @@loaded_tile_sets[name] unless @@loaded_tile_sets[name].nil?
+      # Loads a tile set from a file.
+      def self.tile_set(name, width=nil, height=nil, tile_definitions=nil)
+        @@loaded_tile_sets ||= {}
 
-      # Load this tile set according to what its name was specified as
-      if name.is_a?(Symbol)
-        raise ArgumentError, 'unknown tile set' if TILE_SET_NAMES[name].nil?
-        result = Gosu::Image.load_tiles(*TILE_SET_NAMES[name])
-      elsif name.is_a?(String)
-        raise ArgumentError, 'no dimensions given' if width.nil? || height.nil?
-        result = Gosu::Image.load_tiles(name, width, height)
-      else
-        raise ArgumentError, 'tile set name must be a string or symbol'
+        # See if we have a memoised copy of this tile set
+        return @@loaded_tile_sets[name] unless @@loaded_tile_sets[name].nil?
+
+        # Load this tile set according to what its name was specified as
+        if name.is_a?(Symbol)
+          raise ArgumentError, 'unknown tile set' if TILE_SET_NAMES[name].nil?
+          result = Gosu::Image.load_tiles(*TILE_SET_NAMES[name])
+        elsif name.is_a?(String)
+          raise ArgumentError, 'no dimensions given' if width.nil? || height.nil?
+          result = Gosu::Image.load_tiles(name, width, height)
+        else
+          raise ArgumentError, 'tile set name must be a string or symbol'
+        end
+
+        # Use supplied tile defs for this tileset, or look at the ones we know
+        defs = tile_definitions || TILE_SET_DEFINITIONS[name]
+
+        # Memoise and return this tileset
+        @@loaded_tile_sets[name] = TileSet.new(result, defs)
       end
-
-      # Use supplied tile defs for this tileset, or look at the ones we know
-      defs = tile_definitions || TILE_SET_DEFINITIONS[name]
-
-      # Memoise and return this tileset
-      @@loaded_tile_sets[name] = TileSet.new(result, defs)
     end
   end
 end
--- src/tiles/tile_set.rb
+++ src/tiles/tile_set.rb
@@ -1,19 +1,21 @@
 module ScenicRoute
-  class TileSet
-    attr_reader :images, :tile_definitions
+  module Tiles
+    class TileSet
+      attr_reader :images, :tile_definitions
 
-    def initialize(images, tile_definitions=nil)
-      @images = images
-      @tile_definitions = tile_definitions
-    end
+      def initialize(images, tile_definitions=nil)
+        @images = images
+        @tile_definitions = tile_definitions
+      end
 
-    def tile(name)
-      raise 'no tile definitions' if tile_definitions.nil?
-      images[tile_definitions[name]]
-    end
+      def tile(name)
+        raise 'no tile definitions' if tile_definitions.nil?
+        images[tile_definitions[name]]
+      end
 
-    def inspect
-      puts "#<TileSet: #{images.length} images, #{tile_definitions&.length} defs>"
+      def inspect
+        puts "#<TileSet: #{images.length} images, #{tile_definitions&.length} defs>"
+      end
     end
   end
 end
