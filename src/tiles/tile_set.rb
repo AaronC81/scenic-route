3f72f315f264229340ac879ba7cefeed20f59111
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-17 22:30:22 +0000

    Add TileSet documentation

--- src/tiles/tile_set.rb
+++ src/tiles/tile_set.rb
@@ -1,16 +1,40 @@
 module ScenicRoute
   module Tiles
+    ## 
+    # An ordered collection of images, possibly with names attached to certain
+    # indeces.
     class TileSet
-      attr_reader :images, :tile_definitions
+      ##
+      # @return [Array<Gosu::Image>] The images in this tile set.
+      attr_reader :images
+      
+      ##
+      # @return [Hash] A hash of names as symbols to indeces in {#images}.
+      :tile_definitions
 
+      ##
+      # Create a new tile set.
+      # 
+      # @param [Array<Gosu::Image>] images
+      # @param [Hash] tile_definitions
       def initialize(images, tile_definitions=nil)
         @images = images
         @tile_definitions = tile_definitions
       end
 
+      ##
+      # Retrieve a tile image from the tile set by name.
+      #
+      # @param [Symbol] name The name of the tile, which must be a key in
+      #   {#tile_definitions}.
+      # 
+      # @return [Gosu::Image]
+      #
+      # @raise [RuntimeError] if {#tile_definitions} is nil.
+      # @raise [ArgumentError] if there is no tile with the given name.
       def tile(name)
         raise 'no tile definitions' if tile_definitions.nil?
-        raise "no tile #{name}" if tile_definitions[name].nil?
+        raise ArgumentError, "no tile #{name}" if tile_definitions[name].nil?
         images[tile_definitions[name]]
       end
 
