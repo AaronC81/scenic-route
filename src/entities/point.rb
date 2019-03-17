d6780d7f1a2ee408a04bbd3144581e9f2f35ea1b
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-17 23:34:36 +0000

    Fix and extend Point#heading_to

--- src/entities/point.rb
+++ src/entities/point.rb
@@ -1,19 +1,19 @@
 module ScenicRoute
   module Entities
     Point = Struct.new(:x, :y) do
-      def heading_to(other)
-        case [self.x <=> other.x, self.y <=> other.y]
-        when [0, 1]
-          :north
-        when [0, -1]
-          :south
-        when [1, 0]
-          :west
-        when [-1, 0]
-          :east
-        else
-          nil
-        end
+      def heading_to(other, require_adjacent=false)
+        delta = require_adjacent \
+          ? [self.x - other.x, self.y - other.y]
+          : [self.x <=> other.x, self.y <=> other.y]
+
+        p delta
+
+        {
+          [0, 1] => :north,
+          [0, -1] => :south,
+          [1, 0] => :west,
+          [-1, 0] => :east
+        }[delta]
       end
     end
   end
