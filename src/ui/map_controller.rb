164253f3fa0e9070d6c4f1acf3297aa443bb22b9
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-18 01:59:04 +0000

    Can now hold to draw tracks

--- src/ui/map_controller.rb
+++ src/ui/map_controller.rb
@@ -26,30 +26,37 @@ module ScenicRoute
 
         @map = map
         @origin = origin
+        @drawing = false
       end
 
       ##
-      # Draws the map to the screen.
+      # Draws the map to the screen, and also draw any new track to the screen.
       def draw
         super
 
         map.draw(origin.x, origin.y, 1)
+
+        if @drawing
+          # Check where we were on the map
+          tile_x = (mouse_point.x - origin.x) / map.tile_set.width
+          tile_y = (mouse_point.y - origin.y) / map.tile_set.height
+
+          if tile_x >= 0 && tile_x <= map.width && tile_y >= 0 && tile_y <= map.height
+            map.place_track(Entities::Point.new(tile_x.to_i, tile_y.to_i))
+          end
+        end
       end
 
-      ##
-      # Handles new tracks being drawn.
-      def mouse_down(id, mouse_point)
+      def mouse_down(id)
         super
 
-        # TODO Hold to draw track
-        
-        # Check where we were on the map
-        tile_x = (mouse_point.x - origin.x) / map.tile_set.width
-        tile_y = (mouse_point.y - origin.y) / map.tile_set.height
+        @drawing = true
+      end
 
-        if tile_x >= 0 && tile_x <= map.width && tile_y >= 0 && tile_y <= map.height
-          map.place_track(Entities::Point.new(tile_x.to_i, tile_y.to_i))
-        end
+      def mouse_up(id)
+        super
+
+        @drawing = false
       end
     end
   end
