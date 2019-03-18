01190b00b00b2ceb9e4e98996c55a755953a07bb
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-18 23:47:36 +0000

    Add LandmarkObject#within_bonus_range?

--- src/entities/landmark_object.rb
+++ src/entities/landmark_object.rb
@@ -14,6 +14,13 @@ module ScenicRoute
       def tile_name
         "object_landmark_#{kind}".to_sym
       end
+
+      def within_bonus_range?(query_point)
+        delta_x = (query_point.x - point.x).abs
+        delta_y = (query_point.y - point.y).abs
+
+        delta_x <= 1 && delta_y <= 1
+      end
     end
   end
 end
