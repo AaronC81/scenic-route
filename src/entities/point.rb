d044241396c66718e2ce7a37647c0db7ec58a329
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-19 00:14:10 +0000

    Document Point

--- src/entities/point.rb
+++ src/entities/point.rb
@@ -1,6 +1,18 @@
 module ScenicRoute
   module Entities
-    Point = Struct.new(:x, :y) do
+    ##
+    # A geometric 2D point.
+    Point = Struct.new('Point', :x, :y) do
+      ##
+      # Calculates the 4-directional compass heading to another point.
+      #
+      # @param [Point] other The other point.
+      # @param [Boolean] require_adjacent If true, nil will be returned unless
+      #   {other} is directly adjacent to this point.
+      # 
+      # @return [Symbol?] The compass heading from this point to another, either
+      #   :north, :south, :east, or :west. If it is not directly along one of
+      #   these headings, returns nil.
       def heading_to(other, require_adjacent=false)
         delta = require_adjacent \
           ? [self.x - other.x, self.y - other.y]
@@ -19,6 +31,13 @@ module ScenicRoute
         end
       end
 
+      ##
+      # Returns a copy of this point moved one unit in a direction.
+      #
+      # @param [Symbol] direction The heading in which to move this point; one
+      #   of :north, :south, :east or :west.
+      #
+      # @return [Point] A new, moved point.
       def moved(direction)
         case direction
         when :north
@@ -34,6 +53,12 @@ module ScenicRoute
         end
       end
 
+      ##
+      # Checks whether this point is adjacent to another point.
+      #
+      # @param [Point] other The other point.
+      #
+      # @return [Boolean] True if the points are adjacent, false otherwise.
       def adjacent_to?(other)
         heading_to(other, true) != nil
       end
