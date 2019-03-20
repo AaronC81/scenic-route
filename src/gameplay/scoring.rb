9463ec35ae4e4c2d0b48cdd6acd7179a3471172b
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-20 12:02:28 +0000

    Fix scoring bugs

--- src/gameplay/scoring.rb
+++ src/gameplay/scoring.rb
@@ -13,18 +13,18 @@ module ScenicRoute
       # 
       # @return [Numeric?] The score, or nil if there is no station route.
       def self.score_for_map(map)
-        # TODO: currently only supports one station pair
-        # TODO: doesn't actually return nil on no station route
         stations = map.tile_objects.select { |x| x.is_a?(Entities::StationObject) }
         landmarks = map.tile_objects.select { |x| x.is_a?(Entities::LandmarkObject) }
 
         station_pairs = stations.group_by(&:number)
 
         score = 0
+        connected_pairs = 0
 
         station_pairs.each do |_, (a, b)|
           map.routes.each do |route|
             if route.connects?(a.point.moved(a.orientation), b.point.moved(b.orientation))
+              connected_pairs += 1
               route.points.each do |point|
                 value = 1
                 landmarks.each do |landmark|
@@ -36,7 +36,7 @@ module ScenicRoute
           end
         end
 
-        score
+        connected_pairs == station_pairs.length ? score : nil
       end
     end
   end
