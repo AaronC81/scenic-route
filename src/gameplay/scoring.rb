562a48400b9b8dab0e458d8d3232ef860ef7e56b
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-20 11:43:56 +0000

    Scoring should now support multiple station pairs

--- src/gameplay/scoring.rb
+++ src/gameplay/scoring.rb
@@ -17,19 +17,21 @@ module ScenicRoute
         # TODO: doesn't actually return nil on no station route
         stations = map.tile_objects.select { |x| x.is_a?(Entities::StationObject) }
         landmarks = map.tile_objects.select { |x| x.is_a?(Entities::LandmarkObject) }
-        raise 'too many stations, unimplemented' unless stations.length == 2
+
+        station_pairs = stations.group_by(&:number)
 
         score = 0
 
-        a, b = stations
-        map.routes.each do |route|
-          if route.connects?(a.point.moved(a.orientation), b.point.moved(b.orientation))
-            route.points.each do |point|
-              value = 1
-              landmarks.each do |landmark|
-                value *= 2 if landmark.within_bonus_range?(point)
+        station_pairs.each do |_, (a, b)|
+          map.routes.each do |route|
+            if route.connects?(a.point.moved(a.orientation), b.point.moved(b.orientation))
+              route.points.each do |point|
+                value = 1
+                landmarks.each do |landmark|
+                  value *= 2 if landmark.within_bonus_range?(point)
+                end
+                score += value
               end
-              score += value
             end
           end
         end
