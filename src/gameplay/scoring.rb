794437242bcda8f313f7f9d15f7e5a241400667a
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-20 19:24:08 +0000

    Refactor scoring system and make it work properly for multiple station pairs

--- src/gameplay/scoring.rb
+++ src/gameplay/scoring.rb
@@ -1,5 +1,6 @@
 require_relative '../entities/station_object'
 require_relative '../entities/landmark_object'
+require_relative '../entities/route'
 
 module ScenicRoute
   module Gameplay
@@ -13,30 +14,46 @@ module ScenicRoute
       # 
       # @return [Numeric?] The score, or nil if there is no station route.
       def self.score_for_map(map)
-        stations = map.tile_objects.select { |x| x.is_a?(Entities::StationObject) }
-        landmarks = map.tile_objects.select { |x| x.is_a?(Entities::LandmarkObject) }
+        stations = map.tile_objects(Entities::StationObject)
 
         station_pairs = stations.group_by(&:number)
 
+        scores_for_pairs = station_pairs.map do |num, (a, b)|
+          scores = map.routes.map do |route|
+            route.connects?(a.point.moved(a.orientation),
+              b.point.moved(b.orientation)) ? score_for_route(route) : nil
+          end.compact
+
+          p [num, scores]
+
+          [num, scores.one? ? scores.first : nil]
+        end.to_h
+
+        p scores_for_pairs
+
+        scores_for_pairs.values.all? ? scores_for_pairs.values.min : nil
+      end
+
+      ## 
+      # Calculates a score for a single route. It is the caller's responsibility
+      # to ensure that the route is complete.
+      #
+      # @param [Entities::Route] route The route.
+      #
+      # @return [Numeric?] The score for this route, or nil if the route is
+      #   incomplete.
+      def self.score_for_route(route)
+        landmarks = route.map.tile_objects(Entities::LandmarkObject)
+
         score = 0
-        connected_pairs = 0
-
-        station_pairs.each do |_, (a, b)|
-          map.routes.each do |route|
-            if route.connects?(a.point.moved(a.orientation), b.point.moved(b.orientation))
-              connected_pairs += 1
-              route.points.each do |point|
-                value = 1
-                landmarks.each do |landmark|
-                  value *= 2 if landmark.within_bonus_range?(point)
-                end
-                score += value
-              end
-            end
+        route.points.each do |point|
+          value = 1
+          landmarks.each do |landmark|
+            value *= 2 if landmark.within_bonus_range?(point)
           end
+          score += value
         end
-
-        connected_pairs == station_pairs.length ? score : nil
+        score
       end
     end
   end
