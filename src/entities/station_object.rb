9efb2f80aabf7ddbe367bd447fdf13a69e965d16
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-21 16:40:22 +0000

    Add background and text colours for UI for each station

--- src/entities/station_object.rb
+++ src/entities/station_object.rb
@@ -3,6 +3,16 @@ require_relative 'tile_object'
 module ScenicRoute
   module Entities
     class StationObject < TileObject
+      BACKGROUND_COLORS = {
+        1: 0xFF0000FF,
+        2: 0xFFFF0000
+      }
+
+      TEXT_COLORS = {
+        1: 0xFFFFFFFF
+        2: 0xFFFFFFFF
+      }
+
       attr_reader :orientation, :number
 
       def initialize(point, orientation, number)
