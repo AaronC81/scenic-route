44914f6d92d9caae94c877ffe8ab1e9d55afcbf6
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-25 21:59:57 +0000

    Add callback and some weird floating point fixes to TransitionController

--- src/ui/transition_controller.rb
+++ src/ui/transition_controller.rb
@@ -3,6 +3,8 @@ require_relative 'controller'
 module ScenicRoute
   module UI
     class TransitionController < Controller
+      attr_accessor :covered_callback
+
       def initialize
         super
 
@@ -22,15 +24,28 @@ module ScenicRoute
       end
 
       def draw
+        must_call_covered_callback = false
         if @covering && @opacity < 1
           @opacity += 0.05
+          if @opacity >= 1
+            @opacity = 1 # fix float error
+            must_call_covered_callback = true
+          end
         end
-        if @uncovering && opacity > 0
+        if @uncovering && @opacity > 0
           @opacity -= 0.05
+          if @opacity <= 0
+            @opacity = 0 # fix float error
+          end
         end
 
         Gosu.draw_rect(0, 0, Game::WIDTH, Game::HEIGHT,
           ((@opacity * 255).to_i << 24) + 0x000000, 999)
+
+        if must_call_covered_callback
+          covered_callback&.() 
+          covered_callback = nil
+        end
       end
     end
   end
