207825139133a8075a8d59999cd628d4daa59358
Aaron Christiansen <aaronc20000@gmail.com>
2019-03-27 00:40:04 +0000

    Add level lock checker

--- src/io/level_manager.rb
+++ src/io/level_manager.rb
@@ -20,6 +20,24 @@ module ScenicRoute
       def self.level_paths
         Dir['res/levels/*']
       end
+
+      ##
+      # Finds whether a map is locked. A map is unlocked only when a medal has
+      # been achieved on the map before it.
+      #
+      # @param [Entities::Map] map
+      def self.locked?(map)
+        prev_map_idx = maps.map { |m| m.metadata.id }.index(map.metadata.id) - 1
+        return false if prev_map_idx < 0
+        prev_map = maps[prev_map_idx]
+
+        SaveManager.load_map_state(prev_map)
+
+        score = Gameplay::Scoring.overall_score_for_map(prev_map) || 0
+        medal = map.metadata.medal_for(score)
+
+        medal.nil?
+      end 
     end
   end
 end
