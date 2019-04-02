534b29b0d1249b3c0f300d87b20fddb7b6733cae
Aaron Christiansen <aaronc20000@gmail.com>
2019-04-02 19:12:41 +0100

    Add SampleManager

---
+++ src/io/sample_manager.rb
@@ -0,0 +1,36 @@
+require 'gosu'
+
+module ScenicRoute
+  module IO
+    ##
+    # Handles loading samples from files through symbolic sample names.
+    class SampleManager
+      ##
+      # A map of sample symbols to their filepaths.
+      SAMPLE_PATHS = {
+        hover: 'res/sound/hover.wav',
+        place: 'res/sound/place.wav',
+        select1: 'res/sound/select1.wav',
+        select2: 'res/sound/select2.wav',
+        track_complete: 'res/sound/track_complete.wav'
+      }
+
+      ##
+      # Holds already loaded samples.
+      MEMOIZED_SAMPLES = {}
+
+      ##
+      # Loads an sample from a file given its symbolic name, which must be a key
+      # in {SAMPLE_PATHS}.
+      #
+      # @param [Symbol] name
+      # @return [Gosu::Sample]
+      def self.sample(name)
+        sample_path = SAMPLE_PATHS[name]
+        raise 'no such sample' if sample_path.nil?
+
+        (MEMOIZED_SAMPLES[name] ||= Gosu::Sample.new(sample_path))
+      end
+    end
+  end
+end
